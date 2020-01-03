a8388742ab8708ba178434fbdd287b4dea749df5
jvel07 <joseegaslopez@gmail.com>
2020-01-03 11:02:52 +0100

    resampling

--- signal_processing/resample_wav.py
+++ signal_processing/resample_wav.py
@@ -1,5 +1,6 @@
 import librosa
 from common import util
+import soundfile
 
 work_dir = 'C:/Users/Win10/PycharmProjects/the_speech/audio/wav_anon_75_225/'
 out_dir = 'C:/Users/Win10/PycharmProjects/the_speech/audio/resampled_anon_75_225/'
@@ -8,5 +9,5 @@ orig_audios = util.just_original_75()
 for i in orig_audios:
     y, sr = librosa.load(work_dir + i, sr=16000)
     y_8k = librosa.resample(y, sr, 8000)
-    librosa.output.write_wav(out_dir + i, y_8k, sr)
+    soundfile.write(out_dir + i, y_8k, 8000, 'PCM_16')
     print("Resampled:", i)
