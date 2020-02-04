3502e554d76e80dfc7aca7fd01727a1119a829e9
jvel07 <joseegaslopez@gmail.com>
2020-02-04 15:45:47 +0100

    cv on cold

--- classifiers/cold/cold_helper.py
+++ classifiers/cold/cold_helper.py
@@ -1,9 +1,11 @@
 import numpy as np
 import sklearn as sk
 from imblearn.under_sampling import RandomUnderSampler
-from sklearn import svm
+from sklearn import svm, preprocessing
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.model_selection import StratifiedKFold
+
+from common import data_proc_tools
 
 
 def load_data():
@@ -12,17 +14,17 @@ def load_data():
 
     # Set data directories
     # file_train = work_dir + '/data/cold/train/fisher-13mf-2del-64g-train.fish'
-    file_train = work_dir + '/data/cold/matlab-src/features.fv-mfcc-jose.improved.2.train.txt'
+    file_train = work_dir + '/data/cold/matlab-src/features.fv-mfcc-jose.improved.64.train.txt'
     # file_train = work_dir2 + '/features.fv-mfcc.improved.2.train.txt'
     lbl_train = work_dir + '/data/labels/labels.num.train.txt'
 
     # file_dev = work_dir + '/data/cold/dev/fisher-13mf-2del-64g-dev.fish'
-    file_dev = work_dir + '/data/cold/matlab-src/features.fv-mfcc-jose.improved.2.dev.txt'
+    file_dev = work_dir + '/data/cold/matlab-src/features.fv-mfcc-jose.improved.64.dev.txt'
     # file_dev = work_dir2 + '/features.fv-mfcc.improved.2.dev.txt'
     lbl_dev = work_dir + '/data/labels/labels.num.dev.txt'
 
     # file_test = work_dir + '/data/cold/test/fisher-13mf-2del-64g-test.fish'
-    file_test = work_dir + '/data/cold/matlab-src/features.fv-mfcc-jose.improved.2.test.txt'
+    file_test = work_dir + '/data/cold/matlab-src/features.fv-mfcc-jose.improved.64.test.txt'
     # file_test = work_dir2 + '/features.fv-mfcc.improved.2.test.txt'
     lbl_test = work_dir + '/data/labels/labels.num.test.txt'
 
@@ -49,6 +51,23 @@ def load_data():
     return X_train, Y_train, X_dev, Y_dev, X_test, Y_test, X_combined, Y_combined
 
 
+def powert_data(X, X_dev, X_test):
+    power = preprocessing.PowerTransformer()
+    x_pow = power.fit_transform(X)
+    x_powdev = power.transform(X_dev)
+    x_powtest = power.transform(X_test)
+    return x_pow, x_powdev, x_powtest
+
+
+def normalize_data(X, X_dev, X_test):
+    scaler = preprocessing.Normalizer().fit(X)
+    X_train_norm = scaler.transform(X)
+    X_dev_norm = scaler.transform(X_dev)
+    X_test_norm = scaler.transform(X_test)
+    return X_train_norm, X_dev_norm, X_test_norm
+
+
+
 def resample_data(X, Y, r):
     smote_enn = RandomUnderSampler(random_state=r)
     X_resampled, y_resampled = smote_enn.fit_resample(X, Y)
@@ -57,24 +76,27 @@ def resample_data(X, Y, r):
 
 # train SVM model with stratified cross-validation
 def train_model_stk_cv(X, Y, n_splits, _c):
-    posteriors = []
+    # posteriors = []
     skf = StratifiedKFold(n_splits=n_splits)
     svc = svm.LinearSVC(C=_c, verbose=0, max_iter=3000)  # class_weight='balanced',
+    list_uar = []
     for train_index, test_index in skf.split(X, Y):
         x_train, x_test, y_train, y_test = \
             X[train_index], X[test_index], Y[train_index], Y[test_index]
-        clf = CalibratedClassifierCV(base_estimator=svc, cv=7).fit(x_train, y_train)
-        posteriors.append(clf.predict_proba(x_test))
-        mean_post = np.mean(posteriors, axis=0)
+        # clf = CalibratedClassifierCV(base_estimator=svc, cv=7).fit(x_train, y_train)
+        svc.fit(x_train, y_train)
+        # posteriors.append(svc._predict_proba_lr(x_test))
+        posteriors = svc._predict_proba_lr(x_test)
+        # mean_post = np.mean(posteriors, axis=0)
         # np.savetxt("C:/Users/Win10/PycharmProjects/the_speech/data/cold/posteriors/mean_dev2_posteriors_{}.txt".format(str(c)),
         #            mean_post, fmt='%.7f')
-        print("With:", _c)
-        p0 = mean_post[:, 0:1]
-        p1 = mean_post[:, 1:]
-        y_p = 1 * (p1 > p0)
-        print("Confusion matrix:\n", sk.metrics.confusion_matrix(y_test, y_p))
+        y_p = np.argmax(posteriors, axis=1)
+        # print("Confusion matrix:\n", sk.metrics.confusion_matrix(y_test, y_p))
         one = sk.metrics.recall_score(y_test, y_p, pos_label=0)
         two = sk.metrics.recall_score(y_test, y_p, pos_label=1)
-        print("UAR:", (one + two) / 2, "\n")
+        uar = (one + two) / 2
+        # print("UAR:", uar, "\n")
+        list_uar.append(uar)
 
-    return posteriors
+    uar_tot = np.mean(list_uar)
+    return uar_tot, svc
--- classifiers/cold/cold_svm.py
+++ classifiers/cold/cold_svm.py
@@ -1,16 +1,38 @@
 import numpy as np
 import sklearn as sk
+from sklearn import preprocessing
+from sklearn.utils import shuffle
 
 from classifiers.cold import cold_helper as ch
 
 # Loading Train, Dev, Test and Combined (T+D)
 X_train, Y_train, X_dev, Y_dev, X_test, Y_test, X_combined, Y_combined = ch.load_data()
+# X, Y = shuffle(X_train, Y_train, random_state=0)
+
+X_total = np.concatenate((X_combined, X_test))
+Y_total = np.concatenate((Y_combined, Y_test))
+
+# Normalize data
+scaler = preprocessing.Normalizer().fit(X_total)
+X_train_norm = scaler.transform(X_total)
+X_test_norm = scaler.transform(X_test)
 
 # Resampling, Strat k-fold Cross-val, SVM
 com_values = [1e-5, 1e-4, 1e-3, 1e-2, 0.1, 1, 10]
+# com_values = [1]
+
+X_resampled, Y_resampled = ch.resample_data(X_train_norm, Y_total, r=123456)  # resampling
 for c in com_values:
-    for number in [137, 42, 15986, 4242, 7117, 15, 1, 923, 25, 9656]:
-        X_resampled, Y_resampled = ch.resample_data(X_combined, Y_combined, r=number) # resampling
-        ch.train_model_stk_cv(X_resampled, Y_resampled, 7, c)
+    # for number in [137, 42, 15986, 4242, 7117, 15, 1, 923, 25, 9656]:
+    uar, clf = ch.train_model_stk_cv(X_resampled, Y_resampled, 4, c)
+    print("With:", c, "->", uar)
+
+# Evaluating on test
+posteriors = clf._predict_proba_lr(X_test_norm)
+y_p = np.argmax(posteriors, axis=1)
+print("Confusion matrix:\n", sk.metrics.confusion_matrix(Y_test, y_p))
+one = sk.metrics.recall_score(Y_test, y_p, pos_label=0)
+two = sk.metrics.recall_score(Y_test, y_p, pos_label=1)
+uar_ = (one + two) / 2
 
 
