5b4ba06714c2e6f772c7142197d3ac13bb9006fd
jvel07 <joseegaslopez@gmail.com>
2020-02-24 13:05:22 +0100

    averaging posteriors

--- recipes/cold/cold_svm.py
+++ recipes/cold/cold_svm.py
@@ -10,7 +10,7 @@ from recipes.cold import cold_helper as ch
 work_dir = 'C:/Users/Win10/PycharmProjects/the_speech'  # windows machine
 
 # com_values = [1e-5, 1e-4, 1e-3, 1e-2, 0.1, 1, 10]
-com_values = [0.1]
+com_values = [1e-5]
 
 # retrieving groups for stratified group k-fold CV
 groups = ch.read_utt_spk_lbl()
@@ -19,24 +19,24 @@ groups = ch.read_utt_spk_lbl()
 for g in [64]: #[2, 4, 8, 16, 32, 64, 128]:
     print("CV Process (gaussians):", g)
     # Loading Test, and Combined (Train+Dev)
-    X_test, Y_test, X_combined, Y_combined = ch.load_data(g)
-    # X_test, Y_test, X_combined, Y_combined = ch.load_compare_data()
+    # X_test, Y_test, X_combined, Y_combined = ch.load_data(g)
+    X_test, Y_test, X_combined, Y_combined = ch.load_compare_data()
     # X, Y = shuffle(X_train, Y_train, random_state=0)
 
     # Power Transformer
-    scaler = preprocessing.PowerTransformer().fit(X_combined)
-    X_train_pow = scaler.transform(X_combined)
-    X_test_pow = scaler.transform(X_test)
+    # scaler = preprocessing.PowerTransformer().fit(X_combined)
+    # X_train_pow = scaler.transform(X_combined)
+    # X_test_pow = scaler.transform(X_test)
 
     # X_train_pow, X_test_pow = ch.power_n(X_combined, X_test)
 
     # Normalize data
-    normalizer = preprocessing.Normalizer().fit(X_train_pow)
-    X_train_norm = normalizer.transform(X_train_pow)
-    X_test_norm = normalizer.transform(X_test_pow)
+    normalizer = preprocessing.StandardScaler().fit(X_combined)
+    X_train_norm = normalizer.transform(X_combined)
+    X_test_norm = normalizer.transform(X_test)
 
     # PCA
-    pca = PCA(n_components=0.95) # KernelPCA(kernel='linear', fit_inverse_transform=True, n_components=3500, eigen_solver='arpack')
+    pca = PCA(n_components=0.99) # KernelPCA(kernel='linear', fit_inverse_transform=True, n_components=3500, eigen_solver='arpack')
     X_train_norm = pca.fit_transform(X_train_norm)
     X_test_norm = pca.transform(X_test_norm)
 
@@ -54,8 +54,8 @@ for g in [64]: #[2, 4, 8, 16, 32, 64, 128]:
         two = sk.metrics.recall_score(Y_test, y_p, pos_label=1)
         uar_ = (one + two) / 2
         print(uar_)
-        np.savetxt(work_dir + "/data/cold/posteriors/mean_final_comp_post_{}_{}g.txt".format(
-            str(c), str(g)), posteriors, fmt='%.7f')
+        # np.savetxt(work_dir + "/data/cold/posteriors/mean_final_comp_post_{}_{}g.txt".format(
+        #     str(c), str(g)), posteriors, fmt='%.7f')
 
 
 def uar_scoring(y_true, y_pred, **kwargs):
